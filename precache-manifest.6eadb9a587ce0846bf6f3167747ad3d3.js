b8c7a4d780cbcb5a70562e968fc5550279d43d54
root <root@LAPTOP-9E7P7PNQ.localdomain>
2020-08-18 13:33:26 +0530

    Updates

---
+++ precache-manifest.6eadb9a587ce0846bf6f3167747ad3d3.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a3b679fe21a0e145674c56cc570e6ba",
+    "url": "/github.io/react-notes-app/index.html"
+  },
+  {
+    "revision": "329ea991f62c57ae5ab8",
+    "url": "/github.io/react-notes-app/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "b62c7aac28a66aca1043",
+    "url": "/github.io/react-notes-app/static/css/main.5fa6ee1b.chunk.css"
+  },
+  {
+    "revision": "329ea991f62c57ae5ab8",
+    "url": "/github.io/react-notes-app/static/js/2.028beeec.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/github.io/react-notes-app/static/js/2.028beeec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b62c7aac28a66aca1043",
+    "url": "/github.io/react-notes-app/static/js/main.aa1bade6.chunk.js"
+  },
+  {
+    "revision": "42f5c107ec392e73fbaa",
+    "url": "/github.io/react-notes-app/static/js/runtime-main.df41901d.js"
+  },
+  {
+    "revision": "e6756c34c0faa510f2ec49ff17d60988",
+    "url": "/github.io/react-notes-app/static/media/archive.e6756c34.png"
+  },
+  {
+    "revision": "c93b5ac1352c79a3ac4a0c2bda04dc80",
+    "url": "/github.io/react-notes-app/static/media/logo.c93b5ac1.png"
+  },
+  {
+    "revision": "0f1c59dc9e5ac99103a5dfad852f91b1",
+    "url": "/github.io/react-notes-app/static/media/notes.0f1c59dc.png"
+  },
+  {
+    "revision": "44cc57c0b0dd53b0d9948020533c3cdc",
+    "url": "/github.io/react-notes-app/static/media/reminder.44cc57c0.png"
+  },
+  {
+    "revision": "2c3e6019a6a08b739bb607a4fc17ed38",
+    "url": "/github.io/react-notes-app/static/media/trash.2c3e6019.png"
+  }
+]);
